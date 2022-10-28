0b439944438401a094ceeb9ac07071a1cf859779
Flavio Nunes <flavioborgesnunes@gmail.com>
2022-10-28 15:50:58 -0300

    Projeto finalizado

---
+++ core/forms.py
@@ -0,0 +1,34 @@
+import email
+from pyexpat import model
+from django import forms
+from django.core.mail.message import EmailMessage
+
+from .models import Produto
+
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label='nome', max_length=100)
+    email = forms.EmailField(label='E-mail', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=120)
+    mensagem = forms.CharField(label='mensagem', widget=forms.Textarea())
+
+    def send_email(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        conteudo = f'Nome: {nome}\nE-mail: {email}\nAssunto: {assunto}\nMensagem: {mensagem}'
+
+        mail = EmailMessage(
+            subject= 'E-mail enviado pelo sitema django',
+            body= conteudo,
+            from_email='contato@seudominio.com.br',
+            to=['custodio.patricia@gmail.com'],
+            headers={'Reply-to': email}
+        )
+        mail.send()
+class ProdutoModelForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome','preco','estoque', 'imagem']
---
+++ core/views.py
@@ -0,0 +1,54 @@
+from email import message
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from .forms import ContatoForm, ProdutoModelForm
+from django.contrib import messages
+from .models import Produto
+
+def index(request):
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+
+def contato(request):
+    form = ContatoForm(request.POST or None)
+
+    if  str(request.method) == 'POST':
+        if form.is_valid():
+            form.send_email()
+
+            messages.success(request,'E-mail enviado com sucesso')
+            form = ContatoForm
+        else:
+            message.error(request, 'Erro ao enviar E-mail')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html',context)
+
+
+def produto(request):
+    if str(request.user) != 'AnonymousUser':
+
+        if str(request.method)== 'POST':
+            form =ProdutoModelForm(request.POST, request.FILES )
+            if form.is_valid():
+
+                form.save()
+
+                messages.success(request, 'Produto salvo com sucesso!!')
+                form = ProdutoModelForm()
+            else:
+                message.error(request,  'Erro ao salvar produto!')
+        else:
+            form = ProdutoModelForm()
+        context ={
+            'form':form
+        }
+
+        return render(request, 'produto.html', context)
+    else:
+        return redirect('index')
